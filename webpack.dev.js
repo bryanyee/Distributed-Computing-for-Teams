503c8bc9769d897d7852a5b253d991886a7e0ca4
Bryan Yee <bryan.yee@grandrounds.com>
2019-10-28 09:58:09 -0700

    Build bundle to public folder for production
    https://zeit.co/docs/v2/advanced/platform/frequently-asked-questions#missing-public-directory

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,12 +2,12 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
- devtool: 'source-map',
- mode: 'development',
- watch: true,
- devServer: {
-   historyApiFallback: true,
-   contentBase: './',
-   inline: true
- }
+  devtool: 'source-map',
+  mode: 'development',
+  watch: true,
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    inline: true
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  output: {
+    path: `${__dirname}/public`,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
   mode: 'production',
 });
