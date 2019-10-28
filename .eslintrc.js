b53627d50a4ab22c20aeee4ebca0afc153ad9ed1
Bryan Yee <bryan.yee@grandrounds.com>
2019-10-27 17:49:37 -0700

    Add back eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
-// module.exports = {
-//     "extends": "airbnb",
-//     "installedESLint": true,
-//     "plugins": [
-//         "react",
-//         "jsx-a11y",
-//         "import"
-//     ]
-// };
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2015: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: '2015',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  }
+};
