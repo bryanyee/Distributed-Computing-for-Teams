12685c530e46daf221dd28eaf19db0cc9e7cf74e
Bryan Yee <bryan.yee@grandrounds.com>
2019-10-28 21:23:20 -0700

    Revert "Build bundle to public folder for production"
    This reverts commit 503c8bc9769d897d7852a5b253d991886a7e0ca4.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,5 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  output: {
-    path: `${__dirname}/public`,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
   mode: 'production',
 });
